ab2047786fd92e9519176bb4818c56eff1671175
jpwpdev <jwolf0330@icloud.com>
2024-03-23 00:04:13 -0500

    attempting to fix broken run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the base directory
+BASE_DIR="/home_server"
+
+# Copy the Nginx configuration into sites-available and create a symbolic link in sites-enabled
+sudo cp ${BASE_DIR}/home_server.conf /etc/nginx/sites-available/home_server.conf
+sudo ln -sf /etc/nginx/sites-available/home_server.conf /etc/nginx/sites-enabled/home_server.conf
+
+# Test Nginx configuration and reload if successful
+sudo nginx -t && sudo systemctl reload nginx
+
+# Start the Node.js server using the provided start.sh script
+sudo sh ${BASE_DIR}/server/start.sh
+
+echo "DokuWiki and Node.js server are running."
