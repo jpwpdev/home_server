71b1bf7158b77b376c88b923daf9758800b93470
jpwpdev <jwolf0330@icloud.com>
2024-03-23 12:26:58 -0500

    permissions_fix.sh

--- server/cert.sh
+++ server/cert.sh
@@ -4,5 +4,5 @@ if [[ ! -f server.key || ! -f server.cert ]]; then
     sudo openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=yourdomain.com"
 fi
 
-sudo npm run start
+cd /server && sudo npm run start && cd ..
 # sudo pm2 start server.js --name myapp
