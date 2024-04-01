d6cfa3e9f66377a98f190475d2d9e0896ea92b27
jpwpdev <jwolf0330@icloud.com>
2024-04-01 10:38:05 -0500

    fixing log function error

--- server/cert.sh
+++ server/cert.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-if [[ ! -f /home_server/server.key || ! -f /home_server/server.cert ]]; then
+cd /home_server/server || exit
+
+if [[ ! -f ./server.key || ! -f ./server.cert ]]; then
     sudo openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=US/ST=IL/L=Mundelein/O=FoxHomeNetwork/CN=homenetwork.com"
+    echo created key
 fi
 
-if [[ ! -f /home_server/server.log ]]; then
+if [[ ! -f ./server.log ]]; then
     touch ./server.log
+    echo created log
 fi
 
 # sudo pm2 startup
-cd /home_server/server || exit
+echo running pm2
 sudo pm2 start server.js -i 4
+echo pm2 started
 cd /home_server || exit
 # cd /home_server/server && sudo npm run start && cd /home_server
 # sudo pm2 start server.js --name myapp
