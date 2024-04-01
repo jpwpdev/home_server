8d04afb0d8ff162725452b17474a0fd0b6caf9bc
jpwpdev <jwolf0330@icloud.com>
2024-04-01 10:10:36 -0500

    adding stash for streamlining and logging function for server

--- server/cert.sh
+++ server/cert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ ! -f server.key || ! -f server.cert ]]; then
+if [[ ! -f /home_server/server.key || ! -f /home_server/server.cert ]]; then
     sudo openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=yourdomain.com"
 fi
 
-if [[ ! -f ./server.log ]]; then
+if [[ ! -f /home_server/server.log ]]; then
     touch ./server.log
 fi
 
