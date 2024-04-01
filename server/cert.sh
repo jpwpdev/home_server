61813ddb3437872def862e4f0c807af3388100ad
jpwpdev <jwolf0330@icloud.com>
2024-03-31 23:48:14 -0500

    pushing run pm2 changes

--- server/cert.sh
+++ server/cert.sh
@@ -4,8 +4,9 @@ if [[ ! -f server.key || ! -f server.cert ]]; then
     sudo openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=yourdomain.com"
 fi
 
+sudo pm2 startup
 cd /home_server/server || exit
-sudo pm2 start server.js
+sudo pm2 start server.js -i 4
 cd /home_server || exit
 # cd /home_server/server && sudo npm run start && cd /home_server
 # sudo pm2 start server.js --name myapp
